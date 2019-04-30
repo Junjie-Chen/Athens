702da7749d9266b83802d9646a20f885681a8049
Junjie Chen <junjie.chen18@gmail.com>
2019-04-29 22:02:05 -0400

    feat(component): setup React Router

--- client/index.js
+++ client/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import { Router, Route, hashHistory } from 'react-router';
+import App from './components/App';
 
 const client = new ApolloClient({ dataIdFromObject: o => o.id });
 
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <div>Athens</div>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+        </Route>
+      </Router>
     </ApolloProvider>
   );
 };
