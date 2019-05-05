2950d891bab98c18b35db736426dd86a92fde802
Junjie Chen <junjie.chen18@gmail.com>
2019-05-04 23:17:47 -0400

    feat(component): add, get and set state

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
 
 class AuthForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+
   render() {
     return (
       <div className="row">
         <form className="col s6">
           <div className="input-field">
-            <input placeholder="Email" />
+            <input
+              placeholder="Email"
+              value={this.state.email}
+              onChange={event => this.setState({ email: event.target.value })}
+            />
           </div>
           <div className="input-field">
             <input
               placeholder="Password"
               type="password"
+              value={this.state.password}
+              onChange={event => this.setState({ password: event.target.value })}
             />
           </div>
           <button className="btn">Submit</button>
