d9dea48366d28315ce25efada94d80f7b6d50144
Junjie Chen <junjie.chen18@gmail.com>
2019-05-07 20:20:55 -0400

    feat(component): redirect signed-up user to dashboard

--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
+import { hashHistory } from 'react-router';
 import AuthForm from './AuthForm';
 import query from '../queries/currentUser';
 import mutation from '../mutations/signup';
@@ -9,6 +10,12 @@ class SignupForm extends Component {
     super(props);
 
     this.state = { errors: [] };
+  }
+
+  componentWillUpdate(nextProps) {
+    if (!this.props.data.user && nextProps.data.user) {
+      hashHistory.push('/dashboard');
+    }
   }
 
   onSubmit({ email, password }) {
