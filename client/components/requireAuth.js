5c857eda82483554c94f7f3c71bd18ccb95b95fc
Junjie Chen <junjie.chen18@gmail.com>
2019-05-08 22:34:36 -0400

    feat(component): create RequireAuth

---
+++ client/components/requireAuth.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class RequireAuth extends Component {
+  render() {
+    return <div>Require Authentication</div>;
+  }
+}
+
+export default RequireAuth;
