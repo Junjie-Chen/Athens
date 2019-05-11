5e81beae5ab255de7f58301e5a844c2c6670ab97
Junjie Chen <junjie.chen18@gmail.com>
2019-05-10 21:37:35 -0400

    feat(component): redirect logged-out user to login form

--- client/components/requireAuth.js
+++ client/components/requireAuth.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
+import { hashHistory } from 'react-router';
 import query from '../queries/currentUser';
 
 class RequireAuth extends Component {
+  componentWillUpdate(nextProps) {
+    if (!nextProps.data.loading && !nextProps.data.user) {
+      hashHistory.push('/login');
+    }
+  }
+
   render() {
     return <div>Require Authentication</div>;
   }
